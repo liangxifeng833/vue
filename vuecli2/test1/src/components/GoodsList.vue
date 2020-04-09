0beddb52bcf33d1eaf3a324d8a2b6c576e268fd0
liangxifeng <liangxifeng833@163.com>
2020-04-09 17:56:39 +0800

    second

---
+++ vuecli2/test1/src/components/GoodsList.vue
@@ -0,0 +1,23 @@
+<template>
+    <!---
+        公共的商品列表组件
+        @author: liangxifeng
+        @date: 2020-04-09
+    -->
+    <div name=show>
+        <ul> 
+            <li v-for="good in list">
+                <img v-bind:src="good.img">
+                <p>{{good.goodName}}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+<script type="text/javascript">
+    export default{
+        name:"show",
+        data(){
+            var obj = this;
+        }
+    }
+</script>
